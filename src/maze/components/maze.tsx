80cc90ee40df966ae6a7fe56af0c8d776b0bc156
Neema Tsering <ntvirus333@gmail.com>
2024-04-14 14:24:55 +0545

    add player

--- src/maze/components/maze.tsx
+++ src/maze/components/maze.tsx
@@ -1,14 +1,64 @@
-import { generateMaze } from "../utils/generateMaze.ts";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { Direction } from "../utils/direction.ts";
 
 export type MazeType = number[][];
 
 export interface MazeProps {
   row: number;
   col: number;
+  generatedMaze: MazeType;
 }
 
-const Maze = ({ row, col }: MazeProps) => {
-  const generatedMaze = generateMaze(row, col);
+const Maze = ({ generatedMaze }: MazeProps) => {
+  const [playerDirection, setPlayerDirection] = useState(Direction.NEUTRAL);
+  const playerRef = useRef<Direction>();
+  const currentTimeRef = useRef<number>();
+  const prevTimeRef = useRef<number>();
+  const [playerX, setPlayerX] = useState(0);
+  const [playerY, setPlayerY] = useState(0);
+
+  const handleKeyPress = useCallback((e: KeyboardEvent) => {
+    if (e.key == "w") setPlayerDirection(Direction.UP);
+    else if (e.key == "s") setPlayerDirection(Direction.DOWN);
+    else if (e.key == "a") setPlayerDirection(Direction.LEFT);
+    else if (e.key == "d") setPlayerDirection(Direction.RIGHT);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("keypress", handleKeyPress);
+    return () => window.removeEventListener("keypress", handleKeyPress);
+  }, [handleKeyPress]);
+
+  const updatePosition = (playerDirection: Direction, timedelta: number) => {
+    const multiplier = timedelta * 0.3;
+    if (playerDirection == Direction.UP)
+      setPlayerY((playerY) => playerY - multiplier);
+    if (playerDirection == Direction.DOWN)
+      setPlayerY((playerY) => playerY + multiplier);
+    if (playerDirection == Direction.LEFT)
+      setPlayerX((playerX) => playerX - multiplier);
+    if (playerDirection == Direction.RIGHT)
+      setPlayerX((playerX) => playerX + multiplier);
+  };
+
+  useEffect(() => {
+    playerRef.current = playerDirection;
+  }, [playerDirection]);
+
+  const animatePlayer = (time: number) => {
+    if (prevTimeRef.current && playerRef.current != undefined) {
+      const timedelta = time - prevTimeRef.current;
+      updatePosition(playerRef.current, timedelta);
+    }
+    prevTimeRef.current = time;
+    currentTimeRef.current = requestAnimationFrame(animatePlayer);
+  };
+
+  useEffect(() => {
+    currentTimeRef.current = requestAnimationFrame(animatePlayer);
+    return () => cancelAnimationFrame(currentTimeRef.current!);
+  }, []);
+
   return (
     <>
       <div className="flex flex-col">
@@ -25,6 +75,12 @@ const Maze = ({ row, col }: MazeProps) => {
             </div>
           );
         })}
+        <div
+          className={`size-12 min-h-12 min-w-12 bg-purple-400 flex justify-center items-center absolute`}
+          style={{ transform: `translate(${playerX}px, ${playerY}px)` }}
+        >
+          Player
+        </div>
       </div>
     </>
   );
