cdc6b0189fb3b932e8927d5e492c05fb02b436ba
Neema Tsering <ntvirus333@gmail.com>
2024-05-12 11:03:15 +0545

    add instructions

--- src/maze/components/mazeBuilder.tsx
+++ src/maze/components/mazeBuilder.tsx
@@ -1,6 +1,7 @@
 import { FormEvent, useEffect, useRef, useState } from "react";
 import Maze, { MazeType } from "./maze.tsx";
 import { generateMaze } from "../utils/generateMaze.ts";
+import { Alert, Slide, Snackbar } from "@mui/material";
 
 const MazeBuilder = () => {
   const [row, setRow] = useState<number | undefined>();
@@ -8,6 +9,7 @@ const MazeBuilder = () => {
   const rowRef = useRef<HTMLInputElement>(null);
   const colRef = useRef<HTMLInputElement>(null);
   const [maze, setMaze] = useState<MazeType>();
+  const [openSnackBar, setOpenSnackBar] = useState(false);
 
   const setParsedSize = (
     size: string,
@@ -21,6 +23,7 @@ const MazeBuilder = () => {
     if (row && col && row > 0 && col > 0) {
       const generatedMaze = generateMaze(row, col);
       setMaze(generatedMaze);
+      setOpenSnackBar(true);
     }
   }, [row, col]);
 
@@ -37,6 +40,22 @@ const MazeBuilder = () => {
   return (
     <>
       <div className="fixed top-0 left-0 w-full flex gap-x-5 justify-center items-center bg-gray-500 p-5">
+        <Snackbar
+          open={openSnackBar}
+          onClose={() => setOpenSnackBar(false)}
+          autoHideDuration={4000}
+          anchorOrigin={{ vertical: "top", horizontal: "right" }}
+          TransitionComponent={Slide}
+        >
+          <Alert severity={"info"}>
+            Use <span className="font-semibold">W</span>,{" "}
+            <span className="font-semibold">A</span>,{" "}
+            <span className="font-semibold">S</span>,{" "}
+            <span className="font-semibold">D</span> to control the car.
+            <br />
+            Press <span className="font-semibold">R</span> to reset.
+          </Alert>
+        </Snackbar>
         <form onSubmit={handleSubmit}>
           <div className="flex gap-x-12">
             <div className="flex gap-x-2 items-center">
